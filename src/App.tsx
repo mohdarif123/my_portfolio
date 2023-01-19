29c7bdcd8650247ecbfa210c808dccf1fbf8c877
mohd arif <mohdarifmansoori6063@gmail.com>
2023-01-19 20:54:35 +0530

    routing working

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Home } from "@mui/icons-material";
 import { Box } from "@mui/material";
 import { Route } from "react-router";
-import { BrowserRouter , Routes } from "react-router-dom";
+import { Routes } from "react-router-dom";
 import NavBar from "./Component/NavBar/NavBar";
 import Contact from "./Screen/Contact/Contact";
 import About from "./Screen/About/About";
@@ -12,15 +12,13 @@ function App() {
   return (
     <Box>
       <NavBar />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skill" element={<Skill />} />
-          <Route path="/project" element={<Project />} />
-          <Route path="/contact_us" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skill" element={<Skill />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/contact_us" element={<Contact />} />
+      </Routes>
     </Box>
   );
 }
